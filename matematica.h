f08cf4174f8b8c30fd0cb914791baa0301aa062d
João <joaopedronoronhadutra@gmail.com>
2024-03-13 09:58:02 -0300

    Criando assinaturas das funções no arquivo matematica.h

---
+++ matematica.h
@@ -0,0 +1,26 @@
+/*
+    Calcula o seno de um ângulo em radianos
+
+    @param angulo_radianos Recebe a medida de um ângulo em radianos
+    @return Retorna o seno de angulo_radianos
+
+*/
+float calcular_seno(float angulo_radianos);
+
+/*
+    Calcula o cosseno de um ângulo em radianos
+
+    @param angulo_radianos Recebe a medida de um ângulo em radianos
+    @return Retorna o cosseno de angulo_radianos
+
+*/
+float calcular_cosseno(float angulo_radianos);
+
+/*
+    Calcula a tangente de um ângulo em radianos
+
+    @param angulo_radianos Recebe a medida de um ângulo em radianos
+    @return Retorna a tangente de angulo_radianos
+
+*/
+float calcular_tangente(float angulo_radianos);
